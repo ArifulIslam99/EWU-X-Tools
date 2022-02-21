1fcb4380c9d95998c584eb3e7ad3170fe529b33b
unknown <rawnakjahan1208@gmail.com>
2022-02-22 00:15:21 +0600

    test

--- src/Components/Authentication/Login.js
+++ src/Components/Authentication/Login.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+
 export const Login = () => {
   return (
-    <div>
-        <h3>Hello from login page</h3>
-    </div>
+    
+   <div>
+     <h2>hello</h2>
+    
+   </div>
   )
 }
